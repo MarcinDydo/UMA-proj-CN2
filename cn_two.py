f754debe5c2331b851d7560a97ce25b38d98576a
Marcin Dydo <01149354@pw.edu.pl>
2022-06-12 13:49:14 +0200

    working for large dataset

--- cn_two.py
+++ cn_two.py
@@ -1,32 +1,27 @@
 import pandas as pd
 import numpy as np
 from itertools import combinations
+from queue import Queue
 '''
 This is simple implementation of CN2 algorithm for discrete values of parameters
 '''
-ClassColumnName='klasa'
 
 def is_covered(row, complex):
 	#returns if row of training data is covered by complex k
 	for i, arg in enumerate(row):
-		if i >= len(complex): 
-			break
 		if arg not in complex[i]:
 			return False
 	return True	
 
 def covered_df(data, complex):
 	#returns dataframe covered by complex
-	covered_data=[] 
-	for row in data.iloc:
-		if is_covered(row, complex):	
-			covered_data.append(row)
+	covered_data = [row for row in data.iloc if is_covered(list(row)[:-1],complex)]
 	return pd.DataFrame(covered_data)
 
 def rule_entropy(covered_data):	
 	#returns measure of relevance of complex calculeted from covered df - the higher the better
-	if len(covered_data)==0: return -1
-	class_series = covered_data[ClassColumnName]
+	if len(covered_data) == 0: return -1
+	class_series = covered_data[covered_data.columns[-1]]
 	num_instances = len(class_series)
 	class_counts = class_series.value_counts()
 	class_probabilities = class_counts.divide(num_instances)
@@ -35,93 +30,81 @@ def rule_entropy(covered_data):
 	entropy = plog2p.sum()
 	return entropy
 
-def atomic_complexes(wildcards):
-	#returns all atomic complexes for input data using wildcards
-	atomset = []
+def atomic_combos(wildcards):
+	atomset = []#list of all possible combinations (complexity 2^n-2)
 	for i, wildcard in enumerate(wildcards):
-		combos = [] #list of all possible combinations
+		combos = [] 
 		for l in range(1,len(wildcard)):
 			x=combinations(wildcard, l)
 			combos+=[set(y) for y in x]
-		for c in combos:
-			complex = list(wildcards) #assign wildcards
-			complex[i]=c #assign combination
-			atomset.append(complex)
+		atomset.append(combos)
 	return atomset
-
-def calculate_prim(star,atomic,n):
+	
+def calculate_prim(star,combos):
 	#returns calculated intersection of atomic complexes and star, deletes empty complexes and those that are in star
-	prim=[]
-	for complex in star: 
-		for atomcomplex in atomic: 
-			temp=[]
-			for i in range(0,n):
-				temp.append(atomcomplex[i].intersection(complex[i]))
-			prim.append(temp)
-		
-	j=0
-	while j < len(prim):
-		if prim[j] in star: 
-			del prim[j]
-			continue
-		for i in range(len(prim[j])):
-			if len(prim[j][i])==0:
-				del prim[j]
-				j-=1
-				break
-		j+=1	
+	prim = []
+	for row in star:
+		tmp = list(row)
+		for i, col in enumerate(combos):
+			for atom in col:
+				tmp[i]=row[i].intersection(atom)
+				prim.append(tuple(tmp))
 
+	temp = [x for x in prim if x not in star]
+	prim = [y for y in temp if all(map(lambda x: not len(x)==0, y))]
 	return prim
 
 def compare_complex(trainer, prim, best, significance):
 	#uses heuristics to calculate best complex from set
-	newbest=best
+	newbest = best
+	bestEntropy = rule_entropy(covered_df(trainer,newbest))
 	for complex in prim:
-		if len(covered_df(trainer,complex))>significance and rule_entropy(covered_df(trainer,complex)) > rule_entropy(covered_df(trainer,newbest)): 
+		if len(covered_df(trainer,complex)) <= significance:
+			continue
+		complexEntropy = rule_entropy(covered_df(trainer,complex))
+		if complexEntropy == 0.0:
+			return complex
+		if complexEntropy > bestEntropy: 
+			bestEntropy = complexEntropy
 			newbest = complex
 	return newbest
 
 def best_complex(trainer,prim):
 	if len(prim)==0: return []
 	best=prim[0]
+	bestEntropy = rule_entropy(covered_df(trainer,best))
 	for complex in prim:
-		if rule_entropy(covered_df(trainer,complex)) > rule_entropy(covered_df(trainer,best)):
+		coveredData = covered_df(trainer,complex)
+		complexEntropy = rule_entropy(coveredData)
+		if complexEntropy == 0.0:
+			return [complex]
+		if complexEntropy > bestEntropy:
 			best=complex
+			bestEntropy = complexEntropy
 	return [best]
 
 def print_ruleset(ruleset,inputdata,classColName):
-	ClassColumnName = classColName
-	for rule in ruleset:
+	while not ruleset.empty():
+		rule = ruleset.get()
 		for i, selector in enumerate(rule[0]):
 			print(f"{inputdata.columns[i]} : {selector} ", end='')
-		print(f" -> {ClassColumnName}:{rule[1]}") 
+		print(f" -> {classColName}:{rule[1]}") 
 
-def rules_cn2(inputdata,significance,n,classColName):
-	ClassColumnName = classColName
-	wildcards, star, ruleSet = ([] for i in range(3))
-	for col in inputdata.columns[0:-1]:
-		wildcards.append(set(inputdata[col].unique()))
-	atomicComplexes = atomic_complexes(wildcards)
+def rules_cn2(inputdata,significance,n):
+	ruleSet = Queue()
 	trainingSet = pd.DataFrame(inputdata)
-	while len(trainingSet) > 0:
-		bestComplex=list(wildcards)
-		star.append(list(wildcards))
+	wildcards = tuple(set(trainingSet[col].unique()) for col in inputdata.columns[0:-1])
+	atomicCombos = atomic_combos(wildcards)
+	while len(trainingSet) >0:
+		star = [wildcards]
+		bestComplex=tuple(wildcards)
 		while len(star) > 0:
-			prim = calculate_prim(star,atomicComplexes,n-1)
+			prim = calculate_prim(star,atomicCombos)
 			bestComplex=compare_complex(trainingSet,prim,bestComplex,significance)
 			star = best_complex(trainingSet,prim)
-		category = covered_df(trainingSet, bestComplex)[ClassColumnName].value_counts().idxmax()
-		ruleSet.append([bestComplex,category])
+		category = covered_df(trainingSet, bestComplex)[inputdata.columns[-1]].value_counts().idxmax()
+		ruleSet.put([bestComplex,category])
 		excluded=list(covered_df(trainingSet,bestComplex).index.values)
-		trainingSet.drop(excluded, axis=0, inplace=True)
-	return(ruleSet)
-	
-
-
-	
-	
-	
-	
-	
-	
+		trainingSet.drop(excluded, axis=0, inplace=True)	
+	return ruleSet
 	
--- program.py
+++ program.py
@@ -1,21 +1,35 @@
 import pandas as pd
+from statistics import quantiles
 import cn_two
 import random
+import json
 '''
 Program na ume, wykorzystuje cn2 do generowania zbiorów reguł
 '''
-Path = input("Specify path to training data (csv) ->")
-Significance = int(input("Specify significance (minimum coverage) ->"))
-NumberOfTrees = int(input("Specify number of trees ->"))
-NumberOfAttributes = int(input("Specify number of attributes per tree (optimal is sqrt[n]) ->"))
 
-InputData = pd.read_csv(Path,sep = ';')
+config_path = "config.txt" #input("Config file path ->")
+with open(config_path) as f:
+    data = f.read()
+config = json.loads(data)
+
+InputData = pd.read_csv(config["Path"],sep = ';', dtype=config["DataFormat"])
 GlobalAttributes = list(InputData.columns)[0:-1]
-ClassColumnName = list(InputData.columns)[-1]
-for i in range(0,NumberOfTrees):
-    localAttributes = random.sample(GlobalAttributes,NumberOfAttributes)
-    localAttributes.append(ClassColumnName)
-    localDataFrame = pd.DataFrame(InputData[localAttributes]).sample(len(InputData),replace=True)
-    rules=cn_two.rules_cn2(localDataFrame,Significance,NumberOfAttributes,ClassColumnName)
+ClassName = InputData.columns[-1]
+
+for col in GlobalAttributes:
+        wildcard = list(InputData[col].unique())
+        if len(wildcard) > config["MaxNumOfIntervals"]:
+            bins=[0]
+            bins+=[int(q) for q in quantiles(wildcard,n=config["MaxNumOfIntervals"]+1)]
+            bins[1]-=1
+            bins[-1]=max(wildcard)+1
+            print(bins)
+            InputData[col]=pd.cut(InputData[col],bins=bins,labels=bins[1:],right=False)
+
+for i in range(0,config["NumberOfTrees"]):
+    localAttributes = random.sample(GlobalAttributes,config["NumberOfAttributes"])
+    localAttributes.append(ClassName)
+    localDataFrame = pd.DataFrame(InputData[localAttributes]).sample(config["TrainerSize"],replace=True)
     print(f"Tree {i}")
-    cn_two.print_ruleset(rules,InputData,ClassColumnName)
+    rules=cn_two.rules_cn2(localDataFrame,config["Significance"],config["NumberOfAttributes"])
+    cn_two.print_ruleset(rules,localDataFrame,ClassName)
